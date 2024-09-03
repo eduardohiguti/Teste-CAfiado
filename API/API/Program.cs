46c6fcec0079e6574325a0602b0b51b3d4ad15e3
bosta <bosta@bosta.bosta>
2024-09-02 21:44:43 -0300

    Testando funcionalidades da API

--- API/API/Program.cs
+++ API/API/Program.cs
@@ -1,6 +1,19 @@
+//Minimal APIs em C# 
+//Testar a API
+// - Rest Client - Extensão VSCode
+// - Postman**
+// - Insomnia**
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+//Endpoints - Funcionalidades
+//Requisição - URL e método/verbo HTTP
+app.MapGet("/", () => "Mestre");
+app.MapGet("/mom", () => "Hi mom!");
+app.MapGet("/vampeta", () => "https://assets.portalleodias.com/2024/04/vampetasso-a-trolagem-brasileira-contra-o-preconceito-sofrido-por-vin.webp");
 
+//Exercícios para próxima aula
+// - Criar um endpoint para receber informação pela URL
+
+//dotnet run --project API
 app.Run();
